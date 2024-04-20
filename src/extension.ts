3289b0e8c81d728a7c6ae097a23a91a6035d9635
kazuki235 <fmh13sj@gmail.com>
2024-04-20 22:39:53 +0900

    正式リリース

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,39 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.workspace.onDidChangeTextDocument((e) => {
+  let lastSpace = { row: -1, col: -1, isSpace: false };
+  let disposable = vscode.workspace.onDidChangeTextDocument((event) => {
     const editor = vscode.window.activeTextEditor;
     if (editor && editor.document.languageId === "markdown") {
-      const document = e.document;
+      const changes = event.contentChanges;
 
-      editor.edit((editBuilder) => {
-        for (let li = 0; li < document.lineCount; li++) {
-          const line = document.lineAt(li);
-          const text = line.text;
+      if (changes.length > 0) {
+        const change = changes[0];
+        const newText = change.text;
+        const row = change.range.start.line;
+        const col = change.range.start.character;
 
-          let match;
-          const spaceRegex = /  /g; // Regular expression to match two consecutive spaces
+        console.log(lastSpace);
 
-          // Iterate over matches of two consecutive spaces in the line
-          while ((match = spaceRegex.exec(text)) !== null) {
-            const start = match.index;
-            const end = match.index + 2;
-            const newText = "<br>";
-
-            // Replace two consecutive spaces with <br>
-            const range = new vscode.Range(li, start, li, end);
-            editBuilder.replace(range, newText);
-          }
+        // Detect if the new input is a space
+        if (
+          newText === " " &&
+          lastSpace.isSpace &&
+          lastSpace.row === row &&
+          lastSpace.col === col
+        ) {
+          // Replace two consecutive spaces with <br>
+          const range = new vscode.Range(row, col - 1, row, col + 1);
+          editor.edit((editBuilder) => {
+            editBuilder.replace(range, "<br>");
+          });
+          lastSpace.isSpace = false; // Reset after replacement
+        } else {
+          lastSpace.isSpace = newText === " ";
+          lastSpace.row = row;
+          lastSpace.col = col + newText.length;
         }
-      });
+      }
     }
   });
 
